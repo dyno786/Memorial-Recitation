49fec955bd8a42dc05b5b9715ee160e1624d5fd8
dyno786 <mohammedadris108@gmail.com>
2026-03-17 09:22:26 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 window.APP_CONFIG = {
   supabaseUrl: "https://qbddjirbspxxgtowogna.supabase.co",
-  supabasePublishableKey: "sb_publishable_KrLi7d-CpmxSPZVL3ta9Dg_3QxIiA5h",
-  memorialSlug: "ahmed-khan"
+  supabasePublishableKey: "sb_publishable_KrLi7d-CpmxSPZVL3ta9Dg_3QxIiA5h"
 };
