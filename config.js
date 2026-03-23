8785690ca809fc5d02251a98c041d308c8793af9
dyno786 <mohammedadris108@gmail.com>
2026-03-23 18:36:18 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
   supabaseUrl: "https://qbddjirbspxxgtowogna.supabase.co",
   supabasePublishableKey: "sb_publishable_KrLi7d-CpmxSPZVl3ta9Dg_3QxIiA5h",
-  superAdminEmail: "mohammedadris108@gmail.com" // ← replace with your email
+  superAdminEmail: "mohammedadris198@gmail.com"
 };
